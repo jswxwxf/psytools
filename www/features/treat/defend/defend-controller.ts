5d2b23545a3dcf170e9cb0249328afa86f2629f7
Jeffrey Wang <jwang@JeffreydeMacBook-Pro.local>
2017-01-05 15:29:59 +0800

    init commit

--- www/features/treat/defend/defend-controller.ts
+++ www/features/treat/defend/defend-controller.ts
@@ -14,12 +14,15 @@ class DefendController extends BaseController {
 
   defend;
 
+  dump;
+
   static $inject = ['$scope', '$q', '$timeout', '$ionicScrollDelegate', common.utilService.serviceName, services.defendService.serviceName];
 
   constructor(private $scope, private $q, private $timeout, private $ionicScrollDelegate, private utilService: common.utilService.Service, private defendService: services.defendService.Service) {
     super($scope, utilService);
     super.setModalSrc('thought', 'features/treat/defend/thought.html');
     super.setModalSrc('defend', 'features/treat/defend/defend.html');
+    super.setModalSrc('export', 'features/treat/defend/export.html');
     $scope.$on('modal.shown', () => {
       // $('#content').height($('#treat-defend-import-container').height() - 150);
     });
@@ -51,6 +54,11 @@ class DefendController extends BaseController {
     var found = _.find(this.thought.defends, 'distortion', this.defend.distortion);
     if (!found) this.thought.defends.push(this.defend);
     this.hideModal('defend');
+  }
+
+  exportThought() {
+    this.dump = _.trimLeft(jsyaml.dump(angular.copy(this.thoughts)));
+    this.showModal('export');
   }
 
 }
